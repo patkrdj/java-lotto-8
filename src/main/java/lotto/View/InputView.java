b36bf9535812c863079ff3470cbebf64f55da5b4
patkrdj <taejun154@gmail.com>
2025-10-30 19:18:31 +0900

    refactor(InputView.java): 유닛 테스트를 실행할 수 있도록 validation하는 부분을 분리
    readPurchaseAmount()에서 exception을 체크하는 것과 달리 validatePurchaseAmount로 분리하여 String 값을 인자로 주도록 변경하였다.
    이렇게 함으로써 테스트를 실행했을 때 readLine()하는 부분 없이 유닛테스트를 진행할 수 있도록 하는 것이 목표였다.

--- src/main/java/lotto/View/InputView.java
+++ src/main/java/lotto/View/InputView.java
@@ -5,21 +5,25 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
     public int readPurchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
-        try {
-            int purchaseAmount = 0;
+        while (true) {
             try {
-                purchaseAmount = Integer.parseInt(Console.readLine());
-            } catch (NumberFormatException e) {
-                throw new IllegalArgumentException("숫자값이 아닌 값이 입력되었습니다.");
+                return validatePurchaseAmount(Console.readLine());
+            } catch (IllegalArgumentException e) {
+                System.out.println("[ERROR]: " + e.getMessage());
             }
+        }
+    }
+
+    public int validatePurchaseAmount(String purchaseAmountInput) {
+        try {
+            int purchaseAmount = Integer.parseInt(purchaseAmountInput);
             if (purchaseAmount <= 0)
                 throw new IllegalArgumentException("양수 값이 아닌 값이 입력되었습니다.");
             if (purchaseAmount % 1000 != 0)
                 throw new IllegalArgumentException("1,000원으로 나누어 떨어지지 않는 금액입니다.");
             return purchaseAmount / 1000;
-        } catch (IllegalArgumentException e) {
-            System.out.println("[Error]: " + e.getMessage());
-            return readPurchaseAmount();
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자값이 아닌 값이 입력되었습니다.");
         }
     }
 }
