c2e2efeb2af19c795b0a611a1d31beed677ac16d
patkrdj <taejun154@gmail.com>
2025-10-30 19:23:06 +0900

    test(InputViewTest.java): InputView에 대한 testcase 추가
    복권 구매 금액이 정수값이 아닐때, 음수 값일 때, 공백이 들어 왔을 때, 1000으로 나누어 떨어지지 않을 때의 테스트케이스를 추가 하였음.

--- src/test/java/lotto/View/InputViewTest.java
+++ src/test/java/lotto/View/InputViewTest.java
@@ -1,4 +1,35 @@
 package lotto.View;
 
+import lotto.Lotto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class InputViewTest {
+    @Test
+    void 예외_복권_구매_금액_정수값_아님() {
+        assertThatThrownBy(() -> new InputView().validatePurchaseAmount("ss"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 예외_복권_구매_금액_음수() {
+        assertThatThrownBy(() -> new InputView().validatePurchaseAmount("-2"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 예외_복권_구매_금액_공백() {
+        assertThatThrownBy(() -> new InputView().validatePurchaseAmount(""))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 예외_복권_구매_금액_1000으로_나누어_떨어지지_않음() {
+        assertThatThrownBy(() -> new InputView().validatePurchaseAmount("1200"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
