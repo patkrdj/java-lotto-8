5259608d4ab510942e2f4580a1335c801549776a
patkrdj <taejun154@gmail.com>
2025-11-03 22:06:54 +0900

    fix: ApplicationTest.java의 기능_테스트를 성공하도록 수정
    NumberGenerator.java: sort에서 디버깅이 끊기는 것을 확인하고 그 라인을 삭제하였음.
    LottoGame.java: 로또 설명 텍스트를 수정하였음.

--- src/main/java/lotto/Model/LottoGame.java
+++ src/main/java/lotto/Model/LottoGame.java
@@ -10,7 +10,7 @@ import java.util.stream.Collectors;
 public class LottoGame {
     public enum LottoPrice {
         FIRST(2_000_000_000, "6개 일치"),
-        SECOND(30_000_000, "5개 일치, 보너스 불 일치"),
+        SECOND(30_000_000, "5개 일치, 보너스 볼 일치"),
         THIRD(1_500_000, "5개 일치"),
         FOURTH(50_000, "4개 일치"),
         FIFTH(5_000, "3개 일치"),
--- src/main/java/lotto/Model/NumberGenerator.java
+++ src/main/java/lotto/Model/NumberGenerator.java
@@ -8,7 +8,6 @@ import java.util.List;
 public class NumberGenerator {
     public static List<Integer> generateNumbers() {
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-        numbers.sort(Comparator.naturalOrder());
         return numbers;
     }
 }
